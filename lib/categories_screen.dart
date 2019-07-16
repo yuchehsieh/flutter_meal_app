f991053d837ccbb229496827543992f91a3c1b2e
yuchehseih <hsiehdanny860605@gmail.com>
2019-07-16 15:14:20 +0800

    learn: add font-familys and set the basic theme data

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -6,18 +6,24 @@ import './category_item.dart';
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      children: DUMMY_CATEGORIES.map((category) {
-        return CategoryItem(
-          title: category.title,
-          color: category.color,
-        );
-      }).toList(),
-      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-        maxCrossAxisExtent: 200,
-        childAspectRatio: 3 / 2,
-        crossAxisSpacing: 20,
-        mainAxisSpacing: 20,
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('DeliMeal'),
+      ),
+      body: GridView(
+        padding: const EdgeInsets.all(25),
+        children: DUMMY_CATEGORIES.map((category) {
+          return CategoryItem(
+            title: category.title,
+            color: category.color,
+          );
+        }).toList(),
+        gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
       ),
     );
   }
