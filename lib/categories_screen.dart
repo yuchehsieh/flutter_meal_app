f793b6c219e4e283ebdb91e71a5ebcdd3c4751d9
yuchehseih <hsiehdanny860605@gmail.com>
2019-07-16 14:56:45 +0800

    CupertinoApp START RIGHT HERE

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import './dummy_data.dart';
@@ -6,18 +7,26 @@ import './category_item.dart';
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      children: DUMMY_CATEGORIES.map((category) {
-        return CategoryItem(
-          title: category.title,
-          color: category.color,
-        );
-      }).toList(),
-      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-        maxCrossAxisExtent: 200,
-        childAspectRatio: 3 / 2,
-        crossAxisSpacing: 20,
-        mainAxisSpacing: 20,
+    return CupertinoPageScaffold(
+      navigationBar: CupertinoNavigationBar(
+        middle: const Text('DeliMeal'),
+      ),
+      child: SafeArea(
+        child: GridView(
+          padding: const EdgeInsets.all(25),
+          children: DUMMY_CATEGORIES.map((category) {
+            return CategoryItem(
+              title: category.title,
+              color: category.color,
+            );
+          }).toList(),
+          gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+            maxCrossAxisExtent: 200,
+            childAspectRatio: 3 / 2,
+            crossAxisSpacing: 20,
+            mainAxisSpacing: 20,
+          ),
+        ),
       ),
     );
   }
