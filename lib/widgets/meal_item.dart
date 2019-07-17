1375e5a7b2f19c29c6d776a83c8e021bab8d4ea0
yuchehseih <hsiehdanny860605@gmail.com>
2019-07-17 18:22:07 +0800

    add the rest of design of meal_item.dart

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:meals_app/models/meal.dart';
@@ -17,6 +18,32 @@ class MealItem extends StatelessWidget {
     @required this.affordability,
   });
 
+  String get complexityText {
+    switch (complexity) {
+      case Complexity.Challenging:
+        return 'Challenging';
+      case Complexity.Hard:
+        return 'Challenging';
+      case Complexity.Simple:
+        return 'Simple';
+      default:
+        return 'Unkown';
+    }
+  }
+
+  String get affordabilityText {
+    switch (affordability) {
+      case Affordability.Affordable:
+        return 'Affordable';
+      case Affordability.Luxurious:
+        return 'Expensive';
+      case Affordability.Pricey:
+        return 'Pricey';
+      default:
+        return 'Unkown';
+    }
+  }
+
   void selectMeal() {}
 
   @override
@@ -25,6 +52,8 @@ class MealItem extends StatelessWidget {
       onTap: selectMeal,
       child: Container(
         decoration: BoxDecoration(
+          color: Colors.amberAccent,
+          border: Border.all(width: 1, color: Colors.black87),
           borderRadius: BorderRadius.circular(15),
         ),
         margin: EdgeInsets.all(10),
@@ -43,9 +72,59 @@ class MealItem extends StatelessWidget {
                     width: double.infinity,
                     fit: BoxFit.cover,
                   ),
+                ),
+                Positioned(
+                  right: 10,
+                  bottom: 20,
+                  child: Container(
+                    width: 250,
+                    color: Colors.black54,
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 5,
+                      horizontal: 20,
+                    ),
+                    child: Text(
+                      title,
+                      style: TextStyle(
+                        fontSize: 26,
+                        color: Colors.white,
+                      ),
+                      softWrap: true,
+                      overflow: TextOverflow.fade,
+                    ),
+                  ),
                 )
               ],
-            )
+            ),
+            Padding(
+              padding: const EdgeInsets.all(20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                      Icon(CupertinoIcons.clock),
+                      SizedBox(width: 6),
+                      Text('$duration min')
+                    ],
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Icon(CupertinoIcons.tags),
+                      SizedBox(width: 6),
+                      Text(complexityText)
+                    ],
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Icon(CupertinoIcons.car),
+                      SizedBox(width: 6),
+                      Text(affordabilityText)
+                    ],
+                  )
+                ],
+              ),
+            ),
           ],
         ),
       ),
