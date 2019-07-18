b3b712113c5b65568a567d787cf5bd9422318ed8
yuchehseih <hsiehdanny860605@gmail.com>
2019-07-18 16:50:00 +0800

    add the content inside meal-detail screen

--- lib/screens/meal_detail_screen.dart
+++ lib/screens/meal_detail_screen.dart
@@ -1,18 +1,152 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:meals_app/dummy_data.dart';
 
 class MealDetailScreen extends StatelessWidget {
   static const routeName = '/meal-detail';
 
+  Widget buildSectionTitle(
+    String titleText,
+    BuildContext context,
+    double height,
+  ) {
+    return Container(
+      height: height,
+      margin: const EdgeInsets.symmetric(vertical: 10),
+      child: Text(
+        titleText,
+        style: Theme.of(context).textTheme.title,
+      ),
+    );
+  }
+
+  Widget buildContainer(
+    double height,
+    Widget child,
+  ) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        border: Border.all(
+          color: Colors.grey,
+          width: 1,
+        ),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      margin: const EdgeInsets.all(10),
+      padding: const EdgeInsets.all(10),
+      height: height,
+      width: 300,
+      child: child,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    final mealId = ModalRoute.of(context).settings.arguments as String;
+    final String mealId = ModalRoute.of(context).settings.arguments;
+    final selectedMeal = DUMMY_MEALS.firstWhere((meal) {
+      return meal.id == mealId;
+    });
+
+    final PreferredSizeWidget appBar = CupertinoNavigationBar(
+      middle: Text('Meal Detail - $mealId'),
+    );
+
+    final mediaQuery = MediaQuery.of(context);
+    final double viewHeight = (mediaQuery.size.height -
+        appBar.preferredSize.height -
+        mediaQuery.padding.top);
 
     return CupertinoPageScaffold(
-      navigationBar: CupertinoNavigationBar(
-        middle: Text('Meal Detail - $mealId'),
-      ),
+      navigationBar: appBar,
       child: SafeArea(
-        child: Text('Hello world'),
+        child: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              Container(
+                child: Image.network(
+                  selectedMeal.imageUrl,
+                  fit: BoxFit.cover, // important!
+                ),
+                height: viewHeight * 0.35,
+                width: double.infinity,
+              ),
+              buildSectionTitle(
+                'Ingredients',
+                context,
+                viewHeight * 0.03,
+              ),
+              buildContainer(
+                viewHeight * 0.2,
+                ListView.builder(
+                  itemBuilder: (context, index) {
+                    return Container(
+                      margin: EdgeInsets.symmetric(vertical: 5, horizontal: 5),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(5),
+                        color: CupertinoTheme.of(context).primaryColor,
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(
+                          vertical: 10,
+                          horizontal: 15,
+                        ),
+                        child: Text(selectedMeal.ingredients[index]),
+                      ),
+                    );
+                  },
+                  itemCount: selectedMeal.ingredients.length,
+                ),
+              ),
+              buildSectionTitle(
+                'Steps',
+                context,
+                viewHeight * 0.03,
+              ),
+              buildContainer(
+                viewHeight * 0.2,
+                ListView.builder(
+                  itemBuilder: (context, index) {
+                    return Column(
+                      children: <Widget>[
+                        Row(
+                          children: <Widget>[
+                            Container(
+                              width: 30,
+                              height: 30,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(15),
+                                border: Border.all(width: 1),
+                              ),
+                              child: Center(
+                                child: Text('# ${index + 1}'),
+                              ),
+                            ),
+                            Expanded(
+                              child: Container(
+                                padding: EdgeInsets.symmetric(
+                                  vertical: 10,
+                                  horizontal: 5,
+                                ),
+                                child: Text(
+                                  selectedMeal.steps[index],
+                                  softWrap: true,
+                                  // style: TextStyle(fontSize: 12),
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                        Divider(),
+                      ],
+                    );
+                  },
+                  itemCount: selectedMeal.steps.length,
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
