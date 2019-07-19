b284e435da2ed1c4cb9fd22329990166995bd3c1
yuchehseih <hsiehdanny860605@gmail.com>
2019-07-19 15:24:07 +0800

    flutter-official-turtorial: using CupertinoActionSheet, need to wrap showCupertinoModalPopup() outside of it, IMPORTANTgit add .git add .

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -10,10 +10,49 @@ class CategoriesScreen extends StatefulWidget {
 }
 
 class _CategoriesScreenState extends State<CategoriesScreen> {
-  Future showCupertinoAlertDialog({BuildContext context, Widget child}) async {
-    final dynamic result = await showCupertinoModalPopup<String>(
+  // Future _showCupertinoAlertDialog({BuildContext context, Widget child}) async {
+  //   final dynamic result = await showCupertinoModalPopup<String>(
+  //     context: context,
+  //     builder: (BuildContext context) => child,
+  //   );
+  // }
+
+  void _showCupertinoActionSheet(BuildContext context) {
+    showCupertinoModalPopup(
       context: context,
-      builder: (BuildContext context) => child,
+      builder: (context) {
+        return CupertinoActionSheet(
+          title: Text('Some Actions'),
+          cancelButton: CupertinoActionSheetAction(
+            child: Text('Cancel!!'),
+            onPressed: () {
+              Navigator.pop(context, 'cancel');
+            },
+          ),
+          actions: <Widget>[
+            CupertinoActionSheetAction(
+              child: Text('One - Normal'),
+              onPressed: () {
+                Navigator.pop(context, 1);
+              },
+            ),
+            CupertinoActionSheetAction(
+              child: Text('Two - DefaultAction: true'),
+              onPressed: () {
+                Navigator.pop(context, 2);
+              },
+              isDefaultAction: true,
+            ),
+            CupertinoActionSheetAction(
+              child: Text('Three - DestructiveAction: true'),
+              onPressed: () {
+                Navigator.pop(context, 3);
+              },
+              isDestructiveAction: true,
+            )
+          ],
+        );
+      },
     );
   }
 
@@ -24,7 +63,7 @@ class _CategoriesScreenState extends State<CategoriesScreen> {
         middle: const Text('DeliMeal'),
         leading: CupertinoButton(
           child: Icon(CupertinoIcons.collections_solid),
-          onPressed: () {},
+          onPressed: () => _showCupertinoActionSheet(context),
         ),
       ),
       child: SafeArea(
